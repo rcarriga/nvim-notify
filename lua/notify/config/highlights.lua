fcd476a4b66d3caf475ff44a0aa9203ffdc14693
Lewis Russell <lewis6991@gmail.com>
2021-08-21 21:02:36 +0100

    Redefine highlights when colorscheme changes

--- lua/notify/config/highlights.lua
+++ lua/notify/config/highlights.lua
@@ -1,10 +1,27 @@
-vim.cmd("hi default NotifyERROR guifg=#8A1F1F")
-vim.cmd("hi default NotifyWARN guifg=#79491D")
-vim.cmd("hi default NotifyINFO guifg=#4F6752")
-vim.cmd("hi default NotifyDEBUG guifg=#8B8B8B")
-vim.cmd("hi default NotifyTRACE guifg=#4F3552")
-vim.cmd("hi default NotifyERRORTitle guifg=#F70067")
-vim.cmd("hi default NotifyWARNTitle guifg=#F79000")
-vim.cmd("hi default NotifyINFOTitle guifg=#A9FF68")
-vim.cmd("hi default NotifyDEBUGTitle guifg=#8B8B8B")
-vim.cmd("hi default NotifyTRACETitle guifg=#D484FF")
+local M = {}
+
+function M.setup()
+  vim.cmd[[
+  hi default NotifyERROR guifg=#8A1F1F
+  hi default NotifyWARN guifg=#79491D
+  hi default NotifyINFO guifg=#4F6752
+  hi default NotifyDEBUG guifg=#8B8B8B
+  hi default NotifyTRACE guifg=#4F3552
+  hi default NotifyERRORTitle guifg=#F70067
+  hi default NotifyWARNTitle guifg=#F79000
+  hi default NotifyINFOTitle guifg=#A9FF68
+  hi default NotifyDEBUGTitle guifg=#8B8B8B
+  hi default NotifyTRACETitle guifg=#D484FF
+  ]]
+end
+
+M.setup()
+
+vim.cmd[[
+  augroup nvim_notify
+    autocmd!
+    autocmd ColorScheme * lua require('notify.config.highlights').setup()
+  augroup END
+]]
+
+return M
