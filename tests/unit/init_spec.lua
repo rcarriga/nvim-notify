1a341120afcceea5ca5e2b546363b17a5be8d26e
Rónán Carrigan <rcarriga@tcd.ie>
2021-10-01 09:15:07 +0100

    test: fix history test

--- tests/unit/init_spec.lua
+++ tests/unit/init_spec.lua
@@ -1,12 +1,21 @@
 describe("checking public interface", function()
   local notify = require("notify")
   require("notify").setup({ background_colour = "#000000" })
+  assert:add_formatter(vim.inspect)
 
   describe("notifications", function()
     it("returns all previous notifications", function()
       notify("test", "error")
       local notifs = notify.history()
-      assert.are.same({ { message = { "test" }, level = "ERROR", time = notifs[1].time } }, notifs)
+      assert.are.same({
+        {
+          icon = "",
+          level = "ERROR",
+          message = { "test" },
+          time = notifs[1].time,
+          title = { "", notifs[1].title[2] },
+        },
+      }, notifs)
     end)
   end)
 end)
