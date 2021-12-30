f0d2aaf488fb16e5989820e354db72835c6e473b
Rónán Carrigan <rcarriga@tcd.ie>
2021-12-30 17:33:58 +0000

    fix: set noautocmd in win opts
    Avoids buffer events occurring when a notify window opens (despite not
    entering window) which can interrupt with other plugins
    
    See #46

--- lua/notify/windows/init.lua
+++ lua/notify/windows/init.lua
@@ -54,6 +54,7 @@ function WindowAnimator:push_pending(queue)
     if opacity then
       notif_buf.highlights:set_opacity(opacity)
     end
+    win_opts.noautocmd = true
     local win = api.nvim_open_win(notif_buf:buffer(), false, win_opts)
     vim.wo[win].winhl = "Normal:Normal,FloatBorder:" .. notif_buf.highlights.border
     vim.wo[win].wrap = false
