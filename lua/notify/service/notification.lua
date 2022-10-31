d4dcb0cde37ebe6c3146773ff3a7f4e64fd0b368
Rónán Carrigan <rcarriga@tcd.ie>
2022-10-31 13:42:00 +0000

    feat: disable animation
    Refactors the window animator to run the loop one window at a time
    rather than batching the operations for all windows at once, allowing
    more flexibility in controlling per window animations
    
    See #140

--- lua/notify/service/notification.lua
+++ lua/notify/service/notification.lua
@@ -7,6 +7,7 @@
 ---@field icon string
 ---@field time number
 ---@field width number
+---@field animate boolean
 ---@field hide_from_history boolean
 ---@field keep fun(): boolean
 ---@field on_open fun(win: number) | nil
@@ -43,6 +44,7 @@ function Notification:new(id, message, level, opts, config)
     keep = opts.keep,
     on_open = opts.on_open,
     on_close = opts.on_close,
+    animate = opts.animate ~= false,
     render = opts.render,
     hide_from_history = opts.hide_from_history,
   }
