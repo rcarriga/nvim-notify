a1fbd8d4b2801667dacf9ddaad8daa8dbf37ee09
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-09-10 12:59:22 +0200

    refactor: using snake_case throughout

--- lua/notify/render/wrapped-compact.lua
+++ lua/notify/render/wrapped-compact.lua
@@ -1,6 +1,6 @@
 -- alternative compact renderer for nvim-notify. 
--- Wraps text and adds some padding. (only really to the left, since padding to
--- the right is somehow not display correctly?)
+-- Wraps text and adds some padding (only really to the left, since padding to
+-- the right is somehow not display correctly).
 -- Modified version of https://github.com/rcarriga/nvim-notify/blob/master/lua/notify/render/compact.lua
 --------------------------------------------------------------------------------
 
@@ -20,39 +20,38 @@ end
 ---@param lines string[]
 ---@param max_width number
 ---@return string[]
-local function customWrap(lines, max_width)
-	local wrappedLines = {}
+local function custom_wrap(lines, max_width)
+	local wrapped_lines = {}
 	for _, line in pairs(lines) do
 		local new_lines = split_length(line, max_width)
 		for _, nl in ipairs(new_lines) do
 			nl = nl:gsub("^%s*", " "):gsub("%s*$", " ") -- ensure padding
-			table.insert(wrappedLines, nl)
+			table.insert(wrapped_lines, nl)
 		end
 	end
-	return wrappedLines
+	return wrapped_lines
 end
 
 ---@param bufnr number
 ---@param notif object
 ---@param highlights object
----@param config object plugin configObj
+---@param config object plugin config_obj
 return function (bufnr, notif, highlights, config)
-	local base = require("notify.render.base")
-	local namespace = base.namespace()
+	local namespace = require("notify.render.base").namespace()
 	local icon = notif.icon
 	local title = notif.title[1]
 	local prefix
 
 	-- wrap the text & add spacing
 	local max_width = config.max_width()
-	notif.message = customWrap(notif.message, max_width)
+	notif.message = custom_wrap(notif.message, max_width)
 
-	local defaultTitles = { "Error", "Warning", "Notify" }
-	local hasValidManualTitle = type(title) == "string"
+	local default_titles = { "Error", "Warning", "Notify" }
+	local has_valid_manual_title = type(title) == "string"
 		and #title > 0
-		and not vim.tbl_contains(defaultTitles, title)
+		and not vim.tbl_contains(default_titles, title)
 
-	if hasValidManualTitle then
+	if has_valid_manual_title then
 		-- has title = icon + title as header row
 		prefix = string.format(" %s %s", icon, title)
 		table.insert(notif.message, 1, prefix)
