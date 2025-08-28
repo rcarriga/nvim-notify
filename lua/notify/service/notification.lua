8701bece920b38ea289b457f902e2ad184131a5d
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2025-08-27 22:45:50 -0700

    fix: use correct semver order
    This checks for version 0.11.0 or greater rather than version 11.0.0

--- lua/notify/service/notification.lua
+++ lua/notify/service/notification.lua
@@ -34,15 +34,15 @@ function Notification:new(id, message, level, opts, config)
   if type(title) == "string" then
     title = { title, vim.fn.strftime(config.time_formats().notification, time) }
   end
-  if vim.version.ge(vim.version(), {11, 0, 0}) then
-    vim.validate('message', message, "table")
-    vim.validate('level', level, "string")
-    vim.validate('title', title, "table")
+  if vim.version.ge(vim.version(), { 0, 11, 0 }) then
+    vim.validate("message", message, "table")
+    vim.validate("level", level, "string")
+    vim.validate("title", title, "table")
   else
     vim.validate({
-      message = { message, 'table' },
-      level = { level, 'string' },
-      title = { title, 'table' },
+      message = { message, "table" },
+      level = { level, "string" },
+      title = { title, "table" },
     })
   end
   local notif = {
